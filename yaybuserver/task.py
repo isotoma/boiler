0e19735c64362f1506104463d9d4f63aedf50e93
John Carr <john.carr@isotoma.com>
2011-10-18 13:10:34 +0100

    Some more stubbing

--- yaybuserver/task.py
+++ yaybuserver/task.py
@@ -1,5 +1,6 @@
 from twisted.internet import defer
 
+# Need to think carefully about behaviour when failing and interrupted vs success
 
 class Interrupted(Failure):
     """
@@ -11,7 +12,9 @@ class Interrupted(Failure):
 class Task(object):
 
     """
-    A base class for an interruptible, asynchronous piece of work.
+    Actions performed by Yaybu Server are modelled as a Task. The act of
+    deploying with Yaybu is decoupled from the Task management code to
+    allow other tasks to be orchestrated by Yaybu.
     """
 
     def __init__(self):
@@ -43,12 +46,55 @@ class Task(object):
         return defer.succeed()
 
 
+class CompoundTask(Task):
+
+    """
+    A set of tasks that should be performed in sequence.
+
+    Perhaps you use Fabric to poke Nagios on a second server after deploying to the first.
+    """
+
+    def __init__(self, *tasks):
+        super(Task, self).__init__()
+        self.tasks = tasks
+        self.current = None
+
+    def addTask(self, task):
+        self.tasks.append(task)
+
+    def startNext(self):
+        """ Get the next task and start it. That task is set to call back when it is finished. """
+        if not self.tasks:
+            return
+        self.current = t = self.tasks[0]
+        del self.tasks[0]
+        t.whenDone().addCallback(self.startNext)
+        t.start()
+
+    def start(self):
+        self.startNext()
+
+    def stop(self):
+        """ Clears any remaining subtasks and stops the current one """
+        self.tasks = []
+        return self.current.stop()
+
+    def abort(self):
+        """ Clears any remaining subtasks and aborts the current one """
+        self.tasks = []
+        return self.current.abort()
+
+
 class Tasks(Service):
 
     """
     Manages any tasks that are in progress
 
-    We could potentially rate limit this in the future
+    Currently any queued tasks are started immediately. A list of active
+    tasks is kept so we can stop or abort them during a shutdown.
+
+    We could potentially rate limit this in the future by putting incoming
+    tasks into a queue list.
     """
 
     def __init__(self):
