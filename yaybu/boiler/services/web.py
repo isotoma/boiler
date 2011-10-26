ce39665e53c0e9a9ee6cfdc7f3a803ce14466f9a
John Carr <john.carr@isotoma.com>
2011-10-27 00:52:19 +0100

    Add a delayed resource shim

--- yaybu/boiler/services/web.py
+++ yaybu/boiler/services/web.py
@@ -1,9 +1,37 @@
 
 from twisted.application import strports, service
 from twisted.web import server, static
+from twisted.web.resource import Resource
+from twisted.internet import reactor
 
 
 from yaybu.boiler.service import BaseService
+
+
+class DelayedResource(Resource):
+
+    """
+    A thin shim to allow resource rendering to be more easily asynchronous
+
+    Subclasses should implemented the ``handle`` method and may return a
+    deferred.
+    """
+
+    def handle(self, request):
+        raise NotImplemented(self.handle)
+
+    def render(self, request):
+        d = defer.maybeDeferred(self.handle, request)
+
+        def _(data):
+            if isinstance(data, unicode):
+                data = data.encode("utf-8")
+            request.write(data)
+            request.finish()
+
+        d.addCallbacks(_, request.processingFailed)
+
+        return server.NOT_DONE_YET
 
 
 class WebService(BaseService):
