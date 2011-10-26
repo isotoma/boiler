16fe42bfb7d707d87f7d8e65e63fa09a4bc6f6ab
John Carr <john.carr@isotoma.com>
2011-10-26 21:08:48 +0100

    Make timer instanceable from config

--- yaybu/boiler/services/timer.py
+++ yaybu/boiler/services/timer.py
@@ -1,14 +1,15 @@
 
 from twisted.application.internet import TimerService
+from yaybu.boiler.service import BoilerService
 
-
-class TimedDeployment(TimerService):
+class TimedDeployment(BoilerService):
     """
     Queues a deployment every 5 minutes, but only when the previous one has finished.
     """
 
     def __init__(self, step=5*60):
-        TimerService.__init__(step, self.fire)
+        BoilerService.__init__(self)
+        TimerService(step, self.fire).setServiceParent(self)
 
     def fire(self):
         """ I am called every 5 minutes by TimerService and run some Yay """
